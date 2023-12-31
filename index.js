d570b5669fae812f81dac8fc8bc8a3707376a90e
theawesomeyopro <108738248+theawesomeyopro1@users.noreply.github.com>
2023-12-31 22:40:47 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// Add any JavaScript functionality here if needed
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Add logic for form submission
+    // You can use AJAX to send form data to a server
+    alert('Form submitted!');
+});
